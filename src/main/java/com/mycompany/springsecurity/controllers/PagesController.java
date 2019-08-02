09f4eb364947ed7136051160dedc6a41682a8ea7
tommib <tommib@03TBj-1117.masino.local>
2019-08-02 10:54:04 +0300

    CSRF protection is working now

--- src/main/java/com/mycompany/springsecurity/controllers/PagesController.java
+++ src/main/java/com/mycompany/springsecurity/controllers/PagesController.java
@@ -7,7 +7,6 @@ package com.mycompany.springsecurity.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
--- src/main/java/com/mycompany/springsecurity/controllers/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/mycompany/springsecurity/controllers/configuration/WebSecurityConfiguration.java
@@ -5,10 +5,16 @@
  */
 package com.mycompany.springsecurity.controllers.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  *
@@ -18,6 +24,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Value("${spring.security.username}")
+    private String username;
+    @Value("${spring.security.password}")
+    private String password;
+
+    
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService(){
+        User.UserBuilder user = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(user.username(username).password(password).roles("ADMIN").build());
+        return manager;
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         System.out.println("Olen aktivoitu huutaa Configure");
